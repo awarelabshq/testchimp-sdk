5ff056c6e3e26b6a45dcbd6011974a4e961fb4f1
nuwansamaware <nuwansam@awarelabs.io>
2023-10-21 19:46:54 +1100

    nodejs support added.

---
+++ sdk/backend/nodejs/enableTrackedTests.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { context, trace } = require('@opentelemetry/api');
+
+function enableTrackedTests() {
+  const app = express();
+
+  // Middleware to intercept incoming requests
+  app.use((req, res, next) => {
+    const testName = req.headers['trackedtest.name'];
+    const invocationId = req.headers['trackedtest.invocation_id'];
+    const testType = req.headers['trackedtest.type'];
+
+    if(trace.getSpan(context.active())){
+          // If there's an active span, add the header values to the current span
+        if (testName) {
+            trace.getSpan(context.active()).setAttribute('trackedtest.name', testName);
+        }
+        if (invocationId) {
+            trace.getSpan(context.active()).setAttribute('trackedtest.invocation_id', invocationId);
+        }
+        if (testType) {
+            trace.getSpan(context.active()).setAttribute('trackedtest.type', testType);
+        }
+    }
+    next(); // Continue processing the request
+  });
+
+  return app;
+}
+
+enableTrackedTests();
+
+module.exports = enableTrackedTests;
