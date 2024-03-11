b8aefca6cf9c27860177fd8349ec808687f1bc67
nuwansamaware <nuwansam@awarelabs.io>
2024-03-11 18:24:35 +1100

    Fix minor issues.

--- sdk/automation/cypress/index.js
+++ sdk/automation/cypress/index.js
@@ -13,7 +13,6 @@ module.exports = function enableTracking(disableLogging) {
 
     beforeEach(() => {
         const invocationId=Array(16).fill().map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-        console.log("invocation id: " + invocationId);
         const titleParts = Cypress.currentTest.titlePath;
         const suite = titleParts[0];
         const name = titleParts[1];
@@ -32,11 +31,10 @@ module.exports = function enableTracking(disableLogging) {
 
                     req.headers['trackedtest.suite'] = suite;
                     req.headers['trackedtest.name'] = name;
-                    req.headers['trackedtest.invocation_id'] = invocationId;
                     req.headers['test.type'] = 'cypress';
                     req.headers['traceparent']=traceparent;
                     if (!disableLogging) {
-                        console.log("Tracked Test metadata attached" + " suite: " + suite + " name: " + name + " invocation_id: " + invocationId + " traceparent " + traceparent);
+                        console.log("Tracked Test metadata attached" + " suite: " + suite + " name: " + name + " invocation_id: " + invocationId + " traceparent " + traceparent + " for url: " + req.url);
                     }
                 }
             });
@@ -88,17 +86,19 @@ function extractSuiteAndName(testKey) {
     const suite = testKey.substring(0, delimiterIndex);
     const name = testKey.substring(delimiterIndex + 1);
 
-    return { suite, name };
+    return [ suite, name ];
 }
 
 // Utility function to check assertions
 function checkAssertions(config, invocationId, maxRetries, waitTime, failFast,qualifiedTestName) {
+  cy.log("Checking assertions for " + qualifiedTestName);
   if (maxRetries <= 0) {
-    cy.log("Maximum number of retries exhausted. Skipped backend assertion verification.");
+    console.log("Maximum number of retries exhausted. Skipped backend assertion verification.");
     return Cypress.Promise.resolve(true);
   }
 
-  const {testSuite,testName}=extractSuiteAndName(qualifiedTestName);
+  const [testSuite,testName]=extractSuiteAndName(qualifiedTestName);
+  console.log("trace ids to send: " + Array.from(invocationIdToTraceIdMap.get(invocationId) || new Set()));
   return cy.request({
     method: 'POST',
     url: `${config.api_endpoint}/assert_invocation`,
@@ -118,14 +118,16 @@ function checkAssertions(config, invocationId, maxRetries, waitTime, failFast,qu
    }).then((response) => {
     if (response.body.invocationResult === 'FAILED') {
       cy.log('Backend assertions failed');
-      response.body.assertionResults.forEach((assertionResult) => {
-        cy.log(`Assertion: ${assertionResult.assertionString}`);
-        const leftActualValue = logProtoProperty(assertionResult.leftActualValue);
-        const rightActualValue = logProtoProperty(assertionResult.rightActualValue);
-        cy.log(`Left Actual Value: ${leftActualValue}`);
-        cy.log(`Right Actual Value: ${rightActualValue}`);
-        cy.log(`Result: ${assertionResult.result}`);
-      });
+      if (response.body.assertionResults) {
+          response.body.assertionResults.forEach((assertionResult) => {
+            cy.log(`Assertion: ${assertionResult.assertionString}`);
+            const leftActualValue = logProtoProperty(assertionResult.leftActualValue);
+            const rightActualValue = logProtoProperty(assertionResult.rightActualValue);
+            cy.log(`Left Actual Value: ${leftActualValue}`);
+            cy.log(`Right Actual Value: ${rightActualValue}`);
+            cy.log(`Result: ${assertionResult.result}`);
+          });
+      }
       if (failFast===true) {
         cy.fail('Failing test since backend assertions failed');
         return Cypress.Promise.resolve(false);
@@ -134,14 +136,16 @@ function checkAssertions(config, invocationId, maxRetries, waitTime, failFast,qu
       }
     } else if (response.body.invocationResult === 'PASSED') {
       cy.log('All backend assertions passed');
-      response.body.assertionResults.forEach((assertionResult) => {
-        cy.log(`Assertion: ${assertionResult.assertionString}`);
-        const leftActualValue = logProtoProperty(assertionResult.leftActualValue);
-        const rightActualValue = logProtoProperty(assertionResult.rightActualValue);
-        cy.log(`Left Actual Value: ${leftActualValue}`);
-        cy.log(`Right Actual Value: ${rightActualValue}`);
-        cy.log(`Result: ${assertionResult.result}`);
-      });
+      if (response.body.assertionResults) {
+          response.body.assertionResults.forEach((assertionResult) => {
+            cy.log(`Assertion: ${assertionResult.assertionString}`);
+            const leftActualValue = logProtoProperty(assertionResult.leftActualValue);
+            const rightActualValue = logProtoProperty(assertionResult.rightActualValue);
+            cy.log(`Left Actual Value: ${leftActualValue}`);
+            cy.log(`Right Actual Value: ${rightActualValue}`);
+            cy.log(`Result: ${assertionResult.result}`);
+          });
+      }
       return Cypress.Promise.resolve(true);
     } else {
       // No results found, retry after the specified wait time
@@ -154,7 +158,7 @@ function checkAssertions(config, invocationId, maxRetries, waitTime, failFast,qu
 
 
 // Cypress custom command for verifying backend assertions
-Cypress.Commands.add('verifyBackendAssertions', (waitTime = 120000,retryWaitTime=30000,maxRetryAttempts=2) => {
+Cypress.Commands.add('verifyBackendAssertions', (waitTime = 10000,retryWaitTime=10000,maxRetryAttempts=3) => {
   try {
     cy.fixture('tracked-tests-config.json').then((configData) => {
       const config = configData;
@@ -186,7 +190,7 @@ Cypress.Commands.add('verifyBackendAssertions', (waitTime = 120000,retryWaitTime
 });
 
 // Cypress custom command for verifying backend assertions in all tests within the current suite
-Cypress.Commands.add('verifyAllBackendAssertionsInSuite', (waitTime = 120000, retryWaitTime = 0, maxRetryAttempts = 1) => {
+Cypress.Commands.add('verifyAllBackendAssertionsInSuite', (waitTime = 10000, retryWaitTime = 10000, maxRetryAttempts = 2) => {
   try {
     cy.fixture('tracked-tests-config.json').then((configData) => {
       const config = configData;
