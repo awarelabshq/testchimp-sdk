ef242d23177a5a1f1ac9320c7a35d26b00ce3b7a
nuwansamaware <nuwansam@awarelabs.io>
2024-03-11 22:30:21 +1100

    Update locust sdk to emit traceparent header instead of invocation_id

--- sdk/automation/locust/setup.py
+++ sdk/automation/locust/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tracked-tests-locust',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=['locust'],
     # Include other metadata like author, description, etc.
--- sdk/automation/locust/tracked_tests.py
+++ sdk/automation/locust/tracked_tests.py
@@ -1,19 +1,26 @@
 from locust import TaskSet
-import uuid
+import random
 
 class TrackedTasks(TaskSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.invocation_id = uuid.uuid4()  # Generate a UUID for each task invocation
+        self.invocation_id = ''.join(random.choice('0123456789abcdef') for _ in range(32))  # Generate a random invocation_id
 
     def __getattr__(self, attr):
         def wrapper(*args, **kwargs):
             request_name = f"{self.__class__.__name__}.{attr}"  # Generate request name
+
+            # Generate random trace ID (hexadecimal, 32 characters)
+            trace_id = ''.join(random.choice('0123456789abcdef') for _ in range(32))
+
+            # Construct traceparent header
+            traceparent = f"00-{trace_id}-{self.invocation_id}-01"
             kwargs['headers'] = kwargs.get('headers', {})
-            kwargs['headers']['trackedtest.name'] = request_name  # Add trackedtest.name
-            kwargs['headers']['trackedtest.suite'] = self.__class__.__name__  # Add trackedtest.suite
-            kwargs['headers']['trackedtest.invocation_id'] = str(self.invocation_id)  # Add trackedtest.invocation_id
-            kwargs['headers']['test.type'] = "locust"  # Add trackedtest.name
+            kwargs['headers']['traceparent'] = traceparent
+            kwargs['headers']['trackedtest.name'] = request_name
+            kwargs['headers']['trackedtest.suite'] = self.__class__.__name__
+            kwargs['headers']['test.type'] = "locust"
+
             return getattr(self.client, attr)(*args, **kwargs)
 
         return wrapper
