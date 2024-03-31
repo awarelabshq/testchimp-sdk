491c6f264440da16aab533c7d00b1d1ee39f112a
nuwansamaware <nuwansam@awarelabs.io>
2024-03-31 19:32:18 +1100

    Update release version to 0.0.8793

--- sdk/backend/java-spring/src/main/java/org/trackedtests/sdk/be/java/spring/TrackedTestsConfiguration.java
+++ sdk/backend/java-spring/src/main/java/org/trackedtests/sdk/be/java/spring/TrackedTestsConfiguration.java
@@ -1,14 +1,10 @@
 package org.trackedtests.sdk.be.java.spring;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"org.trackedtests.sdk.be.java.spring"})
 public class TrackedTestsConfiguration {
 
-    @Bean
-    public DefaultRequestCaptureConfig defaultRequestCaptureConfig() {
-        return new DefaultRequestCaptureConfig();
-    }
 }
