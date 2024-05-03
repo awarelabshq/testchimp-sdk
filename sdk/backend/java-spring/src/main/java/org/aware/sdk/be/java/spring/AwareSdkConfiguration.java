237defdf3fc2c5fa6b3b891983ad7fb373322f5e
nuwansamaware <nuwansam@awarelabs.io>
2024-05-03 17:05:47 +1000

    Update config language

--- sdk/backend/java-spring/src/main/java/org/aware/sdk/be/java/spring/AwareSdkConfiguration.java
+++ sdk/backend/java-spring/src/main/java/org/aware/sdk/be/java/spring/AwareSdkConfiguration.java
@@ -2,10 +2,9 @@ package org.aware.sdk.be.java.spring;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @AutoConfiguration
-@ComponentScan(basePackages = {"org.trackedtests.sdk.be.java.spring"})
-public class TrackedTestsConfiguration {
+@ComponentScan(basePackages = {"org.aware.sdk.be.java.spring"})
+public class AwareSdkConfiguration {
 
 }
